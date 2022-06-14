c2426597b97a81ff983cb68adcd00bc01ab3191d
Adrita27 <rahmanadrita27@gmail.com>
2022-06-14 16:34:07 +0600

    labassessment

---
+++ assessment/LABASSESSMENT1.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<title>FORM</title>
+</head>
+<body>
+		<form action="labassessment2.php" method="post">
+			<fieldset>
+				<legend>for name</legend>
+				<table>
+					<tr>
+						<td>username</td>
+						<td><input type="text" name="" value="" ></td>
+					</tr>
+					<tr>
+						<td> </td>
+						<td>
+							<input type="submit" name="" value="Submit">
+							<input type="reset" name="" value="Reset">
+						</td>
+					</tr>
+                </table>
+            </fieldset>
+        </form>
+</body>
+</html>
---
+++ assessment/name.php
@@ -0,0 +1,15 @@
+if(isset($_POST['submit']))
+{
+  $name=$_POST['name'];
+  if($name==null)
+    {
+       header("location:name.htlm1");
+           //echo"<b>Name can not be Null</b>" 
+  elseif($name==[])
+  {
+        echo
+  }
+
+
+    }
+   
